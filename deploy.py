4f734542773c1bcb119af1d3db01d40edb747f69
Mukvin <boyboys520@163.com>
2021-12-06 17:00:10 +0800

    # minor fix

--- deploy.py
+++ deploy.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     logging.config.fileConfig('logging.ini')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--type", required=False, default='destroy', dest='type',
+    parser.add_argument("--type", required=False, default='deploy', dest='type',
                         help="Use 'deploy' to create a cluster or 'destroy' to delete cluster")
     args = parser.parse_args()
     deploy_on_aws(args.type)
